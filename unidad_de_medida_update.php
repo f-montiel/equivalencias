bcb2520c5da7f4f34a293e83f6f95b4119fe5cd0
f-montiel <montiel.facu@gmail.com>
2018-01-13 13:40:15 -0300

    version final

--- unidad_de_medida_update.php
+++ unidad_de_medida_update.php
@@ -2,7 +2,7 @@
 
 include 'connection.php';
 
-$consulta = $pdo->prepare("UPDATE equivalencias.unidades_de_medida SET  (?, ?, ?) WHERE idunidades_de_medida = ?");
+$consulta = $pdo->prepare("UPDATE equivalencias.unidades_de_medida SET unidades_de_medida.nombre = ?, unidades_de_medida.valor = ?, unidades_de_medida.idtipos_um = ? WHERE idunidades_de_medida = ?");
 $nombre = $_POST['nombre'];
 $valor = $_POST['valor'];
 $idunidades_de_medida = $_POST['idunidades_de_medida'];
@@ -10,6 +10,6 @@ $idtipos_um = $_POST['idtipos_um'];
 
 $consulta->execute(array($nombre, $valor, $idtipos_um, $idunidades_de_medida));
 
-header('location:tipo_de_unidad_de_medida_editar.php');
+header('location:index.php');
 
 ?>
